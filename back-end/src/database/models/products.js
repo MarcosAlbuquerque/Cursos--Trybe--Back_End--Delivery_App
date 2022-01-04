b9abf66af119a4e96877bc349dd5e733da55ab1d
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-03 23:01:21 -0300

    [Login - Req1] Cria migration e model de Products

---
+++ back-end/src/database/models/products.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Products = sequelize.define('Products', {
+    name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      // Fonte: https://sequelize.org/v5/manual/data-types.html
+      type: DataTypes.DECIMAL(4, 2),
+      validate: { min: 0.01 },
+    },
+    url_image: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true,
+      }
+    },
+  }, { timestamps: false });
+
+  return Products;
+};
