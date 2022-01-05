75b34ad79cb3439e855a34fbef8d53ab424835ae
Gabriel Castro <kastro.gabriel17@gmail.com>
2022-01-05 18:14:00 -0400

    [Registro - Req 6 - 10] - Forms de registro e validacao

---
+++ front-end/src/Pages/Register.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const stateInitial = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+function Register() {
+  const [stateRegister, setStateRegister] = useState(stateInitial);
+
+  const prefixo = 'common_register__';
+
+  return ( 
+    <>
+      <h2>Cadastro</h2>
+      <form method='post'>
+        <label htmlFor='Nome'>
+          Nome:
+          <input
+            name="name"
+            value={ stateRegister.name }
+            data-testid={ `${prefixo}input-name` }
+            type="text"
+            required
+            onChange={ ({ target }) => setStateRegister({
+              ...stateRegister, name: target.value 
+            }) }
+          />
+        </label>
+        <label htmlFor='Email'>
+          Email:
+          <input
+            name="email"
+            value={ stateRegister.email }
+            data-testid={ `${prefixo}input-email` }
+            type="text"
+            required
+            onChange={ ({ target }) => setStateRegister({
+              ...stateRegister, email: target.value 
+            }) }
+          />
+        </label>
+        <label htmlFor='Password'>
+          Senha:
+          <input
+            name="password"
+            value={ stateRegister.password }
+            data-testid={ `${prefixo}input-password` }
+            type="text"
+            required
+            onChange={ ({ target }) => setStateRegister({
+              ...stateRegister, password: target.value 
+            }) }
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid={ `${prefixo}button-register` }
+        >
+          CADASTRAR
+        </button>
+        <div data-testid={ `${prefixo}element-invalid_register` }>
+          Dados Incorretos
+        </div>
+      </form>
+    </>
+   );
+}
+
+export default Register;
