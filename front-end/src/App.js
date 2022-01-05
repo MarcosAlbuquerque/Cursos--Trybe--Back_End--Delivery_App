3465f067151bd1cfdf986b3543c01b04b03605c6
Marcos Albuquerque <mpdsa20@gmail.com>
2022-01-04 21:49:11 -0300

    [Login - Req1] Inserido redirecionamento de '/' para '/login'

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 import Login from './Pages/Login';
@@ -8,7 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ <Login /> } />
+        <Route path="/" element={ <Navigate to="/login" /> } />
+        <Route path="/login" element={ <Login /> } />
       </Routes>
     </Router>
   );
--- front-end/src/Pages/Login.jsx
+++ front-end/src/Pages/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const Login = () => (<div>Login</div>);
+function Login() {
+  return <h1>Hello World</h1>;
+}
 
 export default Login;
