467d050359e6930df878c4e6897218cdcce32e1a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-06 20:10:01 -0300

    [Register - Req6/7] Adiciona estados aos inputs e add logica do botao de cadastro

--- front-end/src/Pages/Register.jsx
+++ front-end/src/Pages/Register.jsx
@@ -1,68 +1,67 @@
-import React, { useState } from 'react';
-
-const stateInitial = {
-  name: '',
-  email: '',
-  password: '',
-};
+import React from 'react';
+import emailValidation from '../Utils/Validations/emailValidation';
+import passwordValidation from '../Utils/Validations/passwordValidation';
+import nameValidation from '../Utils/Validations/nameValidation';
 
 function Register() {
-  const [stateRegister, setStateRegister] = useState(stateInitial);
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [validCredentials, setValidCredentials] = React.useState(false);
 
   const prefixo = 'common_register__';
+
+  const requestSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  React.useEffect(() => {
+    setValidCredentials(
+      emailValidation(email)
+      && passwordValidation(password)
+      && nameValidation(name),
+    );
+  }, [name, email, password]);
 
   return (
     <>
       <h2>Cadastro</h2>
-      <form method="post">
-        <label htmlFor="Nome">
+      <form onSubmit={ (e) => requestSubmit(e) }>
+        <label htmlFor="name">
           Nome:
           <input
             name="name"
-            value={ stateRegister.name }
             data-testid={ `${prefixo}input-name` }
             type="text"
-            required
-            onChange={ ({ target }) => setStateRegister({
-              ...stateRegister, name: target.value,
-            }) }
+            onChange={ ({ target: { value } }) => setName(value) }
           />
         </label>
-        <label htmlFor="Email">
+        <label htmlFor="email">
           Email:
           <input
             name="email"
-            value={ stateRegister.email }
             data-testid={ `${prefixo}input-email` }
-            type="text"
-            required
-            onChange={ ({ target }) => setStateRegister({
-              ...stateRegister, email: target.value,
-            }) }
+            type="email"
+            onChange={ ({ target: { value } }) => setEmail(value) }
           />
         </label>
-        <label htmlFor="Password">
+        <label htmlFor="password">
           Senha:
           <input
             name="password"
-            value={ stateRegister.password }
             data-testid={ `${prefixo}input-password` }
-            type="text"
-            required
-            onChange={ ({ target }) => setStateRegister({
-              ...stateRegister, password: target.value,
-            }) }
+            type="password"
+            onChange={ ({ target: { value } }) => setPassword(value) }
           />
         </label>
         <button
           type="submit"
           data-testid={ `${prefixo}button-register` }
+          disabled={ !validCredentials }
         >
           CADASTRAR
         </button>
-        <div data-testid={ `${prefixo}element-invalid_register` }>
-          Dados Incorretos
-        </div>
+        <div data-testid={ `${prefixo}element-invalid_register` }>Dados Incorretos</div>
       </form>
     </>
   );
