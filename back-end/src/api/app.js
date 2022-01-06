69788a8ec79e6f160a17b98641440583aa0fe141
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-05 23:23:45 -0300

    [Login - Req1] Corrige erro de ESLint no arquivo app.js

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usersRoutes = require('./routes/UsersRoutes');
 const loginRoutes = require('./routes/LoginRoutes');
+
 const app = express();
 
 app.use('/users', usersRoutes);
