78996714ad921d1096b2d1132e960bee08813126
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-06 12:35:57 -0300

    [Login - req4] Corrige problema de req.body acusando undefined

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const usersRoutes = require('./routes/UsersRoutes');
+const cors = require('cors');
+
 const loginRoutes = require('./routes/LoginRoutes');
+const usersRoutes = require('./routes/UsersRoutes');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.use('/users', usersRoutes);
 app.use('/login', loginRoutes);
--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const CORS = require('cors');
 const app = require('./app');
 
 const port = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(CORS());
 
 app.listen(port, () => {
     console.log(`Api rodando na porta ${port}`);
