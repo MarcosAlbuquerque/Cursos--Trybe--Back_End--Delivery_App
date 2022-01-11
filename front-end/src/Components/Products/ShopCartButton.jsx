831d80a64f0faa328989b021b549d29f3fe2cc96
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-11 17:35:31 -0300

    [Customer - Req16] Adiciona rota para checkout

--- front-end/src/Components/Products/ShopCartButton.jsx
+++ front-end/src/Components/Products/ShopCartButton.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const ShopCartButton = ({ price }) => (
-  <div>
-    <button
-      type="button"
-      data-testid="customer_products__checkout-bottom-value"
-    >
-      {price.replace(/\./, ',')}
-    </button>
-  </div>
-);
+function ShopCartButton({ price }) {
+  const navigate = useNavigate();
+
+  function customerCheckout() {
+    navigate('/customer/checkout');
+  }
+
+  return (
+    <div>
+      <button
+        type="button"
+        disabled={ Number(price) <= 0 }
+        onClick={ () => customerCheckout() }
+        data-testid="customer_products__button-cart"
+      >
+        <span
+          data-testid="customer_products__checkout-bottom-value"
+        >
+          {price.replace(/\./, ',')}
+        </span>
+      </button>
+    </div>);
+}
 
 export default ShopCartButton;
 
