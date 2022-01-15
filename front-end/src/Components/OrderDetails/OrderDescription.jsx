ee3a1a58a81384c31df72249099153074f038f9d
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-15 02:54:23 -0300

    [Customer - Req26] Add req26

--- front-end/src/Components/OrderDetails/OrderDescription.jsx
+++ front-end/src/Components/OrderDetails/OrderDescription.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import formatDate from '../../Utils/formatDate';
 
 const details = 'customer_order_details__element-order-details-label-';
 
@@ -10,11 +11,12 @@ function HeaderTable({ orderInfo }) {
       <h2>Detalhes do pedido</h2>
       <div data-testid={ `${details}order-id` }>{`Pedido N. ${saleId}`}</div>
       <div data-testid={ `${details}seller-name` }>{`P. Vend: ${sellerName}`}</div>
-      <div data-testid={ `${details}order-date` }>{`Data: ${saleDate}`}</div>
+      <div data-testid={ `${details}order-date` }>{`Data: ${formatDate(saleDate)}`}</div>
       <div data-testid={ `${details}delivery-status` }>{`Status: ${status}`}</div>
       <button
         type="button"
         data-testid="customer_order_details__button-delivery-check"
+        disabled={ status !== 'Em Trânsito' }
       >
         Confirmar recebimento
       </button>
